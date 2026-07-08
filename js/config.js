294bdd482ab3c22fa4f841e8a2b834223cba2850
7htma <7hateem@gmail.com>
2026-07-08 14:47:21 +0400

    Add wedding configuration

---
+++ js/config.js
@@ -0,0 +1,16 @@
+window.CONFIG = {
+  groom: "حاتم",
+  bride: "مآثر",
+  displayDate: "٦ أغسطس ٢٠٢٦",
+  weddingDate: "2026-08-06T19:30:00+04:00",
+  eventDurationHours: 4,
+  venue: "قاعة المرايا",
+  location: "سوق الخوض",
+  hosts: ["ربيعة بنت خالد الحارثية", "سميرة بنت يحيى الشبيبية"],
+  mapsUrl: "https://www.google.com/maps/search/?api=1&query=قاعة+المرايا+سوق+الخوض",
+  rsvpEndpoint: "",
+  whatsappNumber: "",
+  adminKey: ""
+};
+
+window.STORAGE_KEY = "hatem_maather_rsvp_entries";
